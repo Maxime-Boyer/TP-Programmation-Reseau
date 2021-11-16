0a04e74210075e8080339d7220af55398b3f1383
quentin-marc <quentinmarc81@gmail.com>
2021-11-16 15:45:44 +0100

    menu 1 v1 non fonctionnelle

--- src/stream_multithread/ClientThread.java
+++ src/stream_multithread/ClientThread.java
@@ -16,6 +16,15 @@ public class ClientThread extends Thread {
     private Socket clientSocket;
     private Serveur serveur;
     private String nomUtilisateur;
+    private EtatsPossibles etat;
+    private boolean afficherMenu;
+
+    private enum EtatsPossibles {
+        MENU_INITIAL,
+        MENU_LISTER_CONVERSATIONS,
+        MENU_LISTER_UTILISATEURS,
+        MENU_CONVERSATION
+    };
 
     /**
      * TODO javadoc Clienthread
@@ -47,14 +56,68 @@ public class ClientThread extends Thread {
             nomUtilisateur = line;
             serveur.connecterUtilisateur(nomUtilisateur);
             System.out.println("\n"+serveur);
+            etat = EtatsPossibles.MENU_INITIAL;
+            afficherMenu = true;
+
+            //message utilisateur connexion reussie
+            socOut.println("Vous êtes connecté avec succès");
 
             while (line != null) {
-                socOut.println("Vous êtes connecté avec succès");
+                if(afficherMenu){
+                    switch (etat){
+                        case MENU_INITIAL:
+                            afficherMenuInitial(socOut);
+                            break;
+                        case MENU_LISTER_CONVERSATIONS:
+                            afficherMenuListerConversations();
+                            break;
+                        case MENU_LISTER_UTILISATEURS:
+                            afficherMenuListerUtilisateurs();
+                            break;
+                        case MENU_CONVERSATION:
+                            afficherMenuConversation();
+                            break;
+                    }
+                }
+
+                // attente d'une entree
+                System.out.println(line);
+
+                // traitement de l'entree
+                /*if(line == "1") {
+                    etat = EtatsPossibles.MENU_LISTER_CONVERSATIONS;
+                }*/
+
             }
             socOut.println("Deconnexion réussie.");
         } catch (Exception e) {
             System.err.println("Error in EchoServer:" + e);
         }
+    }
+
+    public void afficherMenuInitial(PrintStream socOut){
+        socOut.println(" ");
+        socOut.println("Menu initial:");
+        socOut.println("1 - lister les conversations");
+        socOut.println("2 - lister les contacts");
+        socOut.println("3 - entrer dans une conversation");
+        socOut.println("4 - deconnexion");
+        socOut.println(" ");
+        socOut.println("Entrez le numéro correspondant à l'action que vous souhaiter réaliser");
+
+        afficherMenu = false;
+    }
+
+    public void afficherMenuListerConversations(){
+        System.out.println("test1");
+    }
+
+    public void afficherMenuListerUtilisateurs(){
+        System.out.println("test2");
+    }
+
+    public void afficherMenuConversation(){
+        System.out.println("test3");
     }
 
     public String getNomUtilisateur() {
